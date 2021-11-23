69242ef762b7cac9dda6bb9b51b83ad4687932d5
hirnimeshrampuresoftware <71631645+hirnimeshrampuresoftware@users.noreply.github.com>
2021-11-23 19:15:22 +0530

    Update build_manylinux2010.sh

--- tools/build_manylinux2010.sh
+++ tools/build_manylinux2010.sh
@@ -31,10 +31,12 @@ cd ~/ffmpeg_sources
 curl -O -L https://github.com/dmlc/decord/files/5685923/nasm-2.14.02.zip
 unzip nasm-2.14.02.zip
 cd nasm-2.14.02
-yum install -y epel-release
-yum-config-manager --enable epel
-yum install -y autoconf
-yum install -y autogen
+if [ `uname -m` == "aarch64" ]; then
+      yum install -y epel-release
+      yum-config-manager --enable epel
+      yum install -y autoconf
+      yum install -y autogen
+fi      
 ./autogen.sh
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make -j$(nproc)
