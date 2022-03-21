939a636921e340cc62ab7199526fdc1d2aa38f63
hirnimeshrampuresoftware <71631645+hirnimeshrampuresoftware@users.noreply.github.com>
2022-03-21 20:05:43 +0530

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -84,7 +84,6 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
     ],
-    cmdclass={"build_ext": build_ext},
     license='APACHE',
     **setup_kwargs
 )
